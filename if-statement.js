af424e9f7a730a0f0c969b5612fa251cbbfafd72
Earl Duque <eduque@ucdavis.edu>
2018-10-04 15:00:16 -0700

    Added additional ways of doing simple things. Added a file to explain randomizing integers.

--- if-statement.js
+++ if-statement.js
@@ -1,7 +1,19 @@
 var fruit = '6';
 
+// If-Else
+
 if (fruit > 5) {
     console.log('The fruit name has more than five characters.');
 } else {
     console.log('The fruit name has five characters or less.');
+}
+
+// If-ElseIf-Else
+
+if (fruit > 5) {
+    console.log('The fruit name has more than five characters.');
+} else if (fruit < 5) {
+    console.log('The fruit name has less than five characters');
+} else {
+    console.log('The fruit name has exactly five characters');
 }
